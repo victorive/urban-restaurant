012468d605dc8212905ce36d747c92ee3deb2186
Victor Ogugua <victorogugua44@gmail.com>
2024-06-18 23:40:53 +0100

    fix: minor fixes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return redirect('tables');
 });
 
 Route::get('tables', [TableController::class, 'getAllTables'])->name('tables');
